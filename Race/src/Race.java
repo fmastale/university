a9204437120b5199715b96c6bdefa65df4178c06
Filip Mastalerz <filip.mastalerz@gmail.com>
2017-12-03 16:43:53 +0100

    small changes added

--- Race/src/Race.java
+++ Race/src/Race.java
@@ -2,8 +2,20 @@ public class Race {
     public static void main(String[] args) {
         Counter cnt = new Counter(0);
 
-      new IThread(cnt).start();
-      new DThread(cnt).start();
+        Thread one = new IThread(cnt);
+        Thread two = new DThread(cnt);
+
+        one.start();
+        two.start();
+
+
+        try {
+            one.join();
+            two.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
 
         System.out.println("stan=" + cnt.value());
     }
